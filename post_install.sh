bf7fa2b0444e75d33843076670df9fb55be26675
Peter Forret <peter@forret.com>
2022-03-15 16:16:23 +0100

    MOD: post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # cf https://www.addictivetips.com/ubuntu-linux-tips/make-ubuntu-post-installation-script/
 function get_system_info(){
-	suddmidecode -t "${2:-system}" \
+	sudo dmidecode -t "${2:-system}" \
 	| awk -F: -v keyword=${1:-Serial}'
 		function ltrim(s) { sub(/^[ \t\r\n]+/, "", s); return s }
 		function rtrim(s) { sub(/[ \t\r\n]+$/, "", s); return s }
 		function trim(s) { return rtrim(ltrim(s)); }
 		/keyword/ {print $2}
 		'
+}
+
+function random_password(){
+	printf '%x%x\n' $RANDOM $RANDOM
 }
 
 manufacturer=$(get_system_info "Manufacturer")
@@ -28,6 +32,9 @@ sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #sudo flatpak install flatpak1 flatpak2 flatpak3 -y
 
+## Install Ukrianian language support
+# cf https://askubuntu.com/questions/149876/how-can-i-install-one-language-by-command-line
+sudo apt-get install wukrainian 
 
 ## Add User
 sudo adduser ukraine
